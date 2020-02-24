3f1883a0df0af6cee73b5f57ca96a612f9979594
drone-ci <drone@garaza.io>
2020-02-24 19:42:02 +0000

    deploy from drone-ci

---
+++ tags/babylon/index.js
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="generator" content="Hugo 0.18.1" />
+
+  <title>Babylon.Js &middot; Garaža FRI | Študentski tehnološki pospeševalnik</title>
+
+
+  <link href="https://garaza.io/css/bootstrap.css" rel="stylesheet">
+  <link href="https://garaza.io/css/site.css" rel="stylesheet">
+  <link href="https://garaza.io/css/font-awesome.css" rel="stylesheet">
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"></script>
+  <script src="https://garaza.io/js/bootstrap.min.js"></script>
+
+  <link rel="shortcut icon" href="https://garaza.io/img/favicon.ico" type="image/x-icon" />
+  
+
+
+</head>
+
+
+<body>
+
+  <nav class="navbar navbar-fri navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://garaza.io/">
+                <img src="https://garaza.io/img/PNG_GARAZA_LOGOTIP_II.png">
+                <b>Kjer ideje postanejo resničnost</b>
+            </a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <div class="language-selection">
+                    <a href="https://garaza.io/">SI</a> |
+                    <a href="https://garaza.io/en">EN</a>
+                </div>
+                
+                <li><a href="https://garaza.io/ogarazi/">O Garaži</a></li>
+                
+                <li><a href="https://garaza.io/programi">Programi</a></li>
+                
+                <li><a href="https://garaza.io/event/">Dogodki</a></li>
+                
+                <li><a href="https://garaza.io/ekipe/">Ekipe</a></li>
+                
+                <li><a href="https://garaza.io/pridruzise/">Pridruži Se</a></li>
+                
+                <li><a href="https://garaza.io/faq/">FAQ</a></li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+
+  <div id="main">
+
+<div class="header">
+  <h1>Babylon.Js</h1>
+</div>
+
+<div class="content">
+  
+    <article>
+    <div class="skip-small">
+
+        <div class="element element-poudarjen">
+            <div class="element-logotip">
+                <a href="https://garaza.io/event/20170404-3D-babylon/"><img src="" class="logotip"/></a>
+            </div>
+
+            <div class="element-podatki">
+                <div class="element-header">
+                    <a href="https://garaza.io/event/20170404-3D-babylon/"><span class="element-title">Delavnica: Napredne 3D-teme z Babylon.js</span></a>
+                </div>
+
+                <div class="element-body">
+                    <p><div class="post-meta">
+
+  <div>
+    <i class="fa fa-calendar fa-fw"></i>
+    <time>04 Apr 2017, 00:00</time>
+  </div>
+
+  
+
+  
+
+  
+  
+  
+  <div>
+    <i class="fa fa-tags fa-fw"></i>
+    
+      <a class="post-taxonomy-tag" href="https://garaza.io/tags/workshop">workshop</a>&nbsp;&#47;
+    
+      <a class="post-taxonomy-tag" href="https://garaza.io/tags/framework">framework</a>&nbsp;&#47;
+    
+      <a class="post-taxonomy-tag" href="https://garaza.io/tags/delavnice">delavnice</a>&nbsp;&#47;
+    
+      <a class="post-taxonomy-tag" href="https://garaza.io/tags/babylon.js">babylon.js</a>
+    
+  </div>
+  
+  
+
+</div></p>
+                    <p>
+                        <p><strong>Trajanje:</strong> od 9-12</p>
+
+<p><strong>Predava:</strong> Filip Samotorčan, Senior Developer, Agitavit Solutions d.o.o.</p>
+
+<p>Orodje Babylon.js, ki bazira na webGL in Javascriptu, je bilo sicer namenjeno za ustvarjanje spletnih računalniških iger, vendar se v praksi odlično obnese tudi pri izzivih poslovnega sveta. V zadnjih letih se je tako odprlo novo področje uporabe te 3D-knjižnice, ki ga najpogosteje označujejo z izrazom gamification. Gre za uporabo znanj, konceptov in elementov iz sveta iger v kontekste, ki s samimi igrami niso povezani. Zelo pogost primer je razvoj poslovnih aplikacij.</p>
+
+<p></p>
+                    </p>
+                </div>
+                
+                <footer>
+                    <a href="https://garaza.io/event/20170404-3D-babylon/">Preberi več ...<i class="fa fa-angle-double-right fa-fw"></i></a>
+                </footer>
+                
+
+            </div>
+        </div>
+    </div>
+</article>
+
+  
+</div>
+
+</div>
+</div>
+
+<footer>
+    <div class="container">
+        <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <img src="https://garaza.io/img/fri.png" alt="" width="250px">
+                </div>
+                <div class="col-md-4 col-sm-6 fri-info">
+                    <p>UNIVERZA V LJUBLJANI</p>
+                    <p>Fakulteta za računalništvo in informatiko</p>
+                    <p>Večna pot 113, 1000 Ljubljana</p>
+                    <p>Garaža UL FRI</p>
+                    <p>Študentski tehnološki pospeševalnik</p>
+                    <a href="mailto:garaza@fri.uni-lj.si"><i class="fa fa-envelope" aria-hidden="true"></i> garaza@fri.uni-lj.si</a>
+                </div>
+                <div class="col-md-4 col-sm-6 social-info">
+                    <div >
+  <ul class="list-unstyled">
+
+    <li class="pure-menu-item">
+      <a href="https://garaza.io/index.xml"><i class="fa fa-rss fa-fw"></i>RSS</a>
+    </li>
+
+    
+    <li class="pure-menu-item">
+      <a class="pure-menu-link" href="https://twitter.com/garazaFRI" target="_blank"><i class="fa fa-twitter fa-fw"></i>Twitter</a>
+    </li>
+    
+
+    
+    <li class="pure-menu-item">
+      <a class="pure-menu-link" href="https://facebook.com/ulfri" target="_blank"><i class="fa fa-facebook fa-fw"></i>Facebook</a>
+    </li>
+    
+
+    
+    <li class="pure-menu-item">
+      <a class="pure-menu-link" href="https://instagram.com/garazafri" target="_blank"><i class="fa fa-instagram fa-fw"></i>Instagram</a>
+    </li>
+    
+
+    
+    <li class="pure-menu-item">
+      <a class="pure-menu-link" href="https://github.com/garazaFRI" target="_blank"><i class="fa fa-github-alt fa-fw"></i>Github</a>
+    </li>
+    
+
+    
+
+    
+
+  </ul>
+</div>
+                </div>
+            </div>
+    </div>
+</footer>
+
+</body>
+</html>
